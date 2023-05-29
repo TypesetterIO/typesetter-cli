eafc3deaf857cbde0e8b0e59944504118e5e92c6
Aaron Saray <me@aaronsaray.com>
2023-05-29 14:43:42 -0500

    More unit tests

---
+++ tests/Feature/GenerateCommandTest.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use App\Exceptions\ConfigException;
+use Illuminate\Support\Facades\Storage;
+use Tests\TestCase;
+
+class GenerateCommandTest extends TestCase
+{
+    public function xtestSuccessDefaultValues(): void
+    {
+        Storage::fake();
+        Storage::put('config.php', '<?php return [];');
+
+        chdir(__DIR__ . '/../data/default');
+
+        $this->artisan('generate')
+            ->expectsOutput('Welcome to Typesetter!')
+            ->expectsOutput('Done!')
+            ->assertSuccessful();
+
+        Storage::assertExists('my-typeset-book.pdf');
+    }
+
+    public function testSuccessSpecifyingConfigAndOutput(): void
+    {
+        Storage::fake();
+        Storage::put('specific/theme.html', '<style></style>');
+        $theme = Storage::path('specific');
+        Storage::put('specific/my-config.php', '<?php return ["theme" => "' . $theme . '"];');
+
+        $this->artisan('generate', [
+            '--config' => './specific/my-config.php',
+            '--output' => './my-output/file.pdf',
+        ])
+            ->expectsOutput('Welcome to Typesetter!')
+            ->expectsOutput('Done!')
+            ->assertSuccessful();
+
+        Storage::assertExists('my-output/file.pdf');
+    }
+
+    public function testThrowsExceptionIfConfigNotFound(): void
+    {
+        $this->expectException(ConfigException::class);
+        $this->expectExceptionMessage('Cannot find the config file');
+
+        Storage::fake();
+
+        $this->artisan('generate', [
+            '--config' => './my-config.php',
+        ])
+            ->assertFailed();
+    }
+
+    // Is there any suitable way to test the listeners are registered? hrm...
+}
---
+++ tests/Feature/InitCommandTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use Illuminate\Support\Facades\Storage;
+use Tests\TestCase;
+
+class InitCommandTest extends TestCase
+{
+    public function testSuccess(): void
+    {
+        Storage::fake('default');
+
+        Storage::fake('stubs');
+        Storage::disk('stubs')->put('config.php', 'some-config-content');
+        Storage::disk('stubs')->put('content-01.md', 'some-content');
+        Storage::disk('stubs')->put('theme.html', 'theme-html');
+
+        $this->artisan('init')
+            ->expectsOutput('Initializing a Typesetter project!')
+            ->expectsOutput('Success!')
+            ->assertSuccessful();
+
+        Storage::assertExists('config.php');
+        self::assertEquals('some-config-content', Storage::get('config.php'));
+        Storage::assertExists('config.php');
+        self::assertEquals('some-content', Storage::get('content/01.md'));
+        Storage::assertExists('config.php');
+        self::assertEquals('theme-html', Storage::get('my-theme/theme.html'));
+    }
+}
